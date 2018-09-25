9cd2c315e4cda6a4494f18dfad7ece1b7c8b5292
Dmitrii Shulyak <x350sh@gmail.com>
2018-09-25 11:19:58 +0300

    created class - Vector

--- game.js
+++ game.js
@@ -1,1 +1,26 @@
 'use strict';
+
+class Vector {
+  constructor (x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  plus(objectVector) {
+  	if (objectVector insstanceof Vector) {
+  		return new Vector(this.x + objectVector.x, this.y + objectVector.y);
+  	} else {
+		throw 'Можно прибавлять к вектору только вектор типа Vector';
+  	}  		
+  }
+}
+
+
+
+// моя проверка.
+let vec = new Vector(5, 6);
+let vec_1 = new Vector(1, 1);
+
+
+console.log(vec.plus(vec_1));
+console.log(vec instanceof Vector);
